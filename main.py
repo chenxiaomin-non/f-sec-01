426b860372c1ec782e6f5581dd697d15d352c213
chenxiaomin-non <82016572+chenxiaomin-non@users.noreply.github.com>
2022-07-17 20:20:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def ceasar(cipher: str, key: int):
     char_A = ord('A')
     list_cipher = []
     for i in cipher:
+        if i.isalpha() is False:
+            list_cipher.append(i)
+            continue
         value = (ord(i) - char_A + key) % 26 + char_A
         list_cipher.append(chr(value))
     result = ""
@@ -19,5 +22,5 @@ cipher = st.text_input("Ciphertext/Plaintext: ")
 key = st.slider("Key: ", 0, 25, 0)
 
 if st.button("Decrypt/Encrypt"):
-    plaintext = ceasar(cipher, key)
+    plaintext = ceasar(cipher.upper(), key)
     st.write("Your ciphertext/plaintext is: " + plaintext)
