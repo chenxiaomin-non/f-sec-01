fccfa741348ca700dc06ec2910cbf8d04f24fe3c
chenxiaomin-non <82016572+chenxiaomin-non@users.noreply.github.com>
2022-07-17 20:06:32 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def ceasar(cipher: str, key: int):
+    char_A = ord('A')
+    list_cipher = []
+    for i in cipher:
+        value = (ord(i) - char_A + key) % 26 + char_A
+        list_cipher.append(chr(value))
+    result = ""
+    for i in list_cipher:
+        result += i
+    
+    return result
+
+st.title("Tool for today!")
+st.write("Enter ciphertext you want to decrypt or plaintext you want to encrypt:")
+
+cipher = st.text_input("Ciphertext/Plaintext: ")
+key = st.slider("Key: ", 0, 25, 0)
+
+if st.button("Decrypt/Encrypt"):
+    plaintext = ceasar(cipher, key)
+    st.write("Your ciphertext/plaintext is: " + plaintext)
